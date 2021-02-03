4b0d69d56ebb7bdd0bc4751660d0461923c23001
gaoda <gaoda@baidu.com>
2021-02-03 19:26:00 +0800

    add http

--- http.go
+++ http.go
@@ -17,21 +17,21 @@ func HttpBuildQuery(params interface{}) (url string, err error) {
 		return
 	}
 	paramsVal := reflect.ValueOf(params)
-	for i := 0; i < paramsVal.NumField(); i ++  {
+	for i := 0; i < paramsVal.NumField(); i++ {
 		itemKey := paramsTyp.Field(i).Tag.Get("json")
 		itemVal := paramsVal.Field(i).Interface()
 		typ := paramsVal.Field(i).Type()
 		if typ.Kind() == reflect.Map {
 			tmp = append(tmp, httpBuildQuery(itemVal, itemKey))
-		} else if typ.Kind()  == reflect.Slice {
+		} else if typ.Kind() == reflect.Slice {
 			tmp = append(tmp, httpBuildQuery(itemVal, itemKey))
-		} else if typ.Kind()  == reflect.Struct {
+		} else if typ.Kind() == reflect.Struct {
 			tmp = append(tmp, httpBuildQuery(itemVal, itemKey))
-		} else if typ.Kind()  == reflect.String {
+		} else if typ.Kind() == reflect.String {
 			tmp = append(tmp, fmt.Sprintf("%s=%s", itemKey, itemVal.(string)))
-		} else if typ.Kind()  == reflect.Int64 {
+		} else if typ.Kind() == reflect.Int64 {
 			tmp = append(tmp, fmt.Sprintf("%s=%d", itemKey, itemVal.(int64)))
-		} else if typ.Kind()  == reflect.Int {
+		} else if typ.Kind() == reflect.Int {
 			tmp = append(tmp, fmt.Sprintf("%s=%d", itemKey, itemVal.(int)))
 		} else {
 			err = errors.New("TypeError")
@@ -45,8 +45,8 @@ func httpBuildQuery(params interface{}, key string) (url string) {
 	tmp := []string{}
 	typ := reflect.TypeOf(params)
 	val := reflect.ValueOf(params)
-	if typ.Kind() == reflect.Map || typ.Kind()  == reflect.Struct {
-		for i := 0; i < val.NumField(); i ++ {
+	if typ.Kind() == reflect.Map || typ.Kind() == reflect.Struct {
+		for i := 0; i < val.NumField(); i++ {
 			itemKey := typ.Field(i).Tag.Get("json")
 			itemTyp := val.Field(i).Type()
 			itemVal := val.Field(i).Interface()
@@ -56,16 +56,16 @@ func httpBuildQuery(params interface{}, key string) (url string) {
 				tmp = append(tmp, httpBuildQuery(itemVal, fmt.Sprintf("%s[%s]", key, itemKey)))
 			} else if itemTyp.Kind() == reflect.Array {
 				tmp = append(tmp, httpBuildQuery(itemVal, fmt.Sprintf("%s[%d]", key, i)))
-			} else if itemTyp.Kind()  == reflect.String {
+			} else if itemTyp.Kind() == reflect.String {
 				tmp = append(tmp, fmt.Sprintf("%s[%s]=%s", key, itemKey, itemVal.(string)))
-			} else if itemTyp.Kind()  == reflect.Int64 {
+			} else if itemTyp.Kind() == reflect.Int64 {
 				tmp = append(tmp, fmt.Sprintf("%s[%s]=%d", key, itemKey, itemVal.(int64)))
-			} else if itemTyp.Kind()  == reflect.Int {
+			} else if itemTyp.Kind() == reflect.Int {
 				tmp = append(tmp, fmt.Sprintf("%s[%s]=%d", key, itemKey, itemVal.(int)))
 			}
 		}
-	} else if typ.Kind()  == reflect.Slice {
-		for i := 0; i < val.Len(); i ++ {
+	} else if typ.Kind() == reflect.Slice {
+		for i := 0; i < val.Len(); i++ {
 			itemKey := strconv.Itoa(i)
 			itemTyp := val.Index(i).Type()
 			itemVal := val.Index(i).Interface()
@@ -75,11 +75,11 @@ func httpBuildQuery(params interface{}, key string) (url string) {
 				tmp = append(tmp, httpBuildQuery(itemVal, fmt.Sprintf("%s[%s]", key, itemKey)))
 			} else if itemTyp.Kind() == reflect.Array {
 				tmp = append(tmp, httpBuildQuery(itemVal, fmt.Sprintf("%s[%d]", key, i)))
-			} else if itemTyp.Kind()  == reflect.String {
+			} else if itemTyp.Kind() == reflect.String {
 				tmp = append(tmp, fmt.Sprintf("%s[%s]=%s", key, itemKey, itemVal.(string)))
-			} else if itemTyp.Kind()  == reflect.Int64 {
+			} else if itemTyp.Kind() == reflect.Int64 {
 				tmp = append(tmp, fmt.Sprintf("%s[%s]=%s", key, itemKey, itemVal.(int64)))
-			} else if itemTyp.Kind()  == reflect.Int {
+			} else if itemTyp.Kind() == reflect.Int {
 				tmp = append(tmp, fmt.Sprintf("%s[%s]=%d", key, itemKey, itemVal.(int)))
 			}
 		}
@@ -90,31 +90,30 @@ func httpBuildQuery(params interface{}, key string) (url string) {
 }
 
 type TestReq struct {
-	Name string `json:"name"`
+	Name     string   `json:"name"`
 	Comments []string `json:"comments"`
-	Imgs []Img `json:"imgs"`
+	Imgs     []Img    `json:"imgs"`
 }
 
 type Img struct {
-	Src string
-	Height int
+	Src    string `json:"src"`
+	Height int    `json:"height"`
 }
-
 
 func main() {
 	imgs := []Img{
 		{
-			Src: "apple",
+			Src:    "apple",
 			Height: 100,
-		},{
-			Src: "banana",
+		}, {
+			Src:    "banana",
 			Height: 200,
 		},
 	}
 	test := TestReq{
-		Name: "gaoda",
+		Name:     "gaoda",
 		Comments: []string{"hello", "nihao"},
-		Imgs: imgs,
+		Imgs:     imgs,
 	}
 	a, e := HttpBuildQuery(test)
 	fmt.Println(a)
