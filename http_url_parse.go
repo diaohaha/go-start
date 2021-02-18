7d603d0362b5fe623c602a28a02532c418ebd071
gaoda <gaoda@baidu.com>
2021-02-18 16:04:15 +0800

    leetcode

--- http_url_parse.go
+++ http_url_parse.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/url"
-	"strings"
 )
 
 func main()  {
 
 	// url参数中有换行符是解析要替换
-	urlStr := "/service?user_id=&vote_id=&user=abc\n和孙悟空&&source=1"
-	newS := strings.Replace(urlStr, "\n", "\\n", -1)
-	_, _ = url.Parse(newS)
+	urlStr := "http://www.baidu.com?param=123"
+	//newS := strings.Replace(urlStr, "\n", "\\n", -1)
+	i, err := url.Parse(urlStr)
+	log.Println(i.Path)
+	log.Println(i.RawPath)
+	log.Println(i)
+	log.Println(err)
+
+	params := url.Values{
+		"user": []string{"哈哈哈哈\n哈哈哈"},
+	}
+
+	urlParams := params.Encode()
+	log.Println(params.Encode())
+
+	params1, err := url.ParseQuery(urlParams)
+	fmt.Println(params1.Get("user"))
+
 }
---
+++ leetcode/leetcode589.go
@@ -0,0 +1,38 @@
+package leetcode
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Children []*Node
+ * }
+ */
+
+type Node struct {
+	Val      int
+	Children []*Node
+}
+
+func preorder(root *Node) []int {
+	res := []int{}
+	toTraverse := []*Node{}
+	if root == nil {
+		return res
+	}
+	res = append(res, root.Val)
+	for _, child := range root.Children {
+		toTraverse = append(toTraverse, child)
+	}
+	for true {
+		if len(toTraverse) == 0 {
+			break
+		}
+		nowRoot := toTraverse[0]
+		// 切片操作
+		toTraverse = toTraverse[1:]
+		res = append(res, nowRoot.Val)
+		// ... 语法
+		toTraverse = append(nowRoot.Children, toTraverse...)
+	}
+	return res
+}
